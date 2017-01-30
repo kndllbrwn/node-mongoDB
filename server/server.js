5ec1ac9409da047f4cc75c3ee9533b206e0fa75f
kndllbrwn <kndllbrwn1914@gmail.com>
2017-01-29 23:11:46 -0500

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,81 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: true
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+   var todo = new Todo({
+       text: req.body.text
+   });
+
+   todo.save().then((doc) => {
+       res.send(doc);
+   }, (e) => {
+       res.status(400).send(e);
+   });
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-
-var newTodo2 = new Todo({
-    text: 'Walk dog',
-    completed: false,
-    completedAt: 1500 
-});
-
-var otherTodo = new Todo({
-    text: ' Edit this code '
-});
-
-var User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-
-var newUser2 = new User({
-    name: "Ken",
-    email: "ken2@example" 
-});
-
-newUser2.save().then((doc) => {
-    console.log('Saved new User', doc)
-}, (e) => {
-    console.log('Unable to save new user', e)
+app.listen(3000, () => {
+    console.log('Started on port 3000')
 })
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// })
-
-// newTodo2.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// })
-
-// otherTodo.save().then((doc) => {
-//     console.log('Saved todo', doc)
-// }, (e) => {
-//     console.log('Unable to save todo')
-// })
-
