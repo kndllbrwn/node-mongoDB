8c4f51a51720d8cf361853dee2b1b8be8a445a8d
kndllbrwn <kndllbrwn1914@gmail.com>
2017-02-06 01:00:00 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,36 +1,45 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+password = '123abc!';
 
-var token = jwt.sign(data, 'abc132');
-console.log(token);
-var decoded = jwt.verify(token, 'abc132');
-console.log('decoded', decoded);
-
-// var message = 'I am user number 3';
-// var hash = SHA256(message).toString();
-
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
 // var data = {
-//     id: 4
-// };
-// var token = {
-//     data,
-//     hash: SHA256(JSON.stringify(data) + 'someSecret').toString()
+//     id: 10
 // }
 
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString()
+// var token = jwt.sign(data, 'abc132');
+// console.log(token);
+// var decoded = jwt.verify(token, 'abc132');
+// console.log('decoded', decoded);
 
-// var resultHash = SHA256(JSON.stringify(token.data)+ 'someSecret').toString();
+// // var message = 'I am user number 3';
+// // var hash = SHA256(message).toString();
 
-// if (resultHash === token.hash) {
-//     console.log('Data was not changed');
-// } else {
-//     console.log('Data was changed. Do not trust!');
-// }
+// // console.log(`Message: ${message}`);
+// // console.log(`Hash: ${hash}`);
+
+// // var data = {
+// //     id: 4
+// // };
+// // var token = {
+// //     data,
+// //     hash: SHA256(JSON.stringify(data) + 'someSecret').toString()
+// // }
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString()
+
+// // var resultHash = SHA256(JSON.stringify(token.data)+ 'someSecret').toString();
+
+// // if (resultHash === token.hash) {
+// //     console.log('Data was not changed');
+// // } else {
+// //     console.log('Data was changed. Do not trust!');
+// // }
